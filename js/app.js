699e4e35a447d6d62892c016bfa0b95d55dbabca
Kyle Vassella <kylevassella@gmail.com>
2016-09-12 23:43:51 -0500

    J&C Realty
    J&C Realty initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $('.slickSlides').slick({
+        arrows: true,
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+  });
+});
+	
---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.slick').slick({
+    arrows: true
+  });
+});
+
+$('.fade').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
