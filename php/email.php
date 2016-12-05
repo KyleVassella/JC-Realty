9c1d4cb841c86fa6fdbef3f63adc2f402c28030f
Kyle Vassella <kylevassella@gmail.com>
2016-12-05 05:26:50 -0600

    Changed  to  for GitHub servers.

--- php/email.php
+++ php/email.php
@@ -1,9 +1,11 @@
+<!-- $_POST has been temporarily replaced with $_GET because $_POST is not allowed on Github servers. -->
+
 <?php
-    $to = $_POST['kylevassella@gmail.com'];
-	$subject = $_POST['subject'];
-    $message = $_POST['message'];
-    $name = $_POST['name'];
-    $email = $_POST['email'];
+    $to = $_GET['kylevassella@gmail.com'];
+	$subject = $_GET['subject'];
+    $message = $_GET['message'];
+    $name = $_GET['name'];
+    $email = $_GET['email'];
 
     mail($to, $subject, $message, "From: " . $name, $email);
 
