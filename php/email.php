7c8cf547cc5a4865444e080fd9663f43be998c2a
Kyle Vassella <kylevassella@gmail.com>
2016-12-05 06:01:08 -0600

    PHP for contact form.

--- php/email.php
+++ php/email.php
@@ -1,11 +1,9 @@
-<!-- $_POST has been temporarily replaced with $_GET because $_POST is not allowed on Github servers. -->
-
 <?php
-    $to = $_GET['kylevassella@gmail.com'];
-	$subject = $_GET['subject'];
-    $message = $_GET['message'];
-    $name = $_GET['name'];
-    $email = $_GET['email'];
+    $to = $_POST['kylevassella@gmail.com'];
+	$subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
 
     mail($to, $subject, $message, "From: " . $name, $email);
 
