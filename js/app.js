19e1f2cbea54e00ed4ff4d0382a598b62364a243
Kyle Vassella <kylevassella@gmail.com>
2017-02-12 22:45:20 -0600

    .

--- js/app.js
+++ js/app.js
@@ -1,13 +1,36 @@
 
-$(document).ready(function(){
-  $('.slickSlides').slick({
-        arrows: true,
-      dots: true,
-      infinite: true,
-      speed: 500,
-      fade: true,
-      cssEase: 'linear'
-  });
-});
+// $(document).ready(function(){
+//   $('.slickSlides').slick({
+//         arrows: true,
+//       dots: true,
+//       infinite: true,
+//       speed: 500,
+//       fade: true,
+//       cssEase: 'linear'
+//   });
+// });
 	
-	
+// 	
+
+// var $emailInput = $('.email-input');
+// var $nameInput = $('.name-input');
+// var $emailLabel = $('.email-label');
+
+
+
+
+
+// function validateForm() {
+
+//   function validateEmail(email) {
+//       var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//       return regex.test(email);
+//   }
+
+//    if (validateEmail($emailInput.val())) {
+
+//    } else {
+//    	$emailLabel.text('Please enter a valid email.');
+//    }
+
+// }
